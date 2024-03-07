ac26b737b6842a6ff545b5b2c47e8bb54f803fcf
FalsettiM <90410065+FalsettiM@users.noreply.github.com>
2024-03-06 22:03:35 -0700

    Removed attendee from code
    No longer making a distinction between attendee and organizer

--- src/OOPsIPushedToMain/app/src/main/java/com/oopsipushedtomain/MainActivity.java
+++ src/OOPsIPushedToMain/app/src/main/java/com/oopsipushedtomain/MainActivity.java
@@ -5,11 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AttendeeProfileActivity.class);
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intent);
             }
         });
