ef1676e108ddcc0598cfec0148b55b51cc757b31
AidanGironella <aidangironella@gmail.com>
2024-03-08 01:47:19 -0700

    can view event announcements

--- src/OOPsIPushedToMain/app/src/main/java/com/oopsipushedtomain/Event.java
+++ src/OOPsIPushedToMain/app/src/main/java/com/oopsipushedtomain/Event.java
@@ -60,6 +60,16 @@ public class Event implements Serializable {
         this.posterUrl = posterUrl;
         this.attendeeLimit = attendeeLimit; // Optional
     }
+    public Event(String eventId, String title, String startTime, String endTime, String description, String location, String posterUrl, int attendeeLimit) {
+        this.eventId = eventId;
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+        this.location = location; // Optional
+        this.posterUrl = posterUrl;
+        this.attendeeLimit = attendeeLimit; // Optional
+    }
 
     /**
      * No-argument constructor so that Event can be deserialized
